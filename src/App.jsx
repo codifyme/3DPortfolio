47a1a396327a44f2e0bf1ef36cd973e89c752505
Angel Cochachez <angelcochachez@Angels-MacBook-Pro.local>
2023-03-21 10:10:24 -0700

    Installation, added reactsnap scroll effect and designed all jsx sections

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import styled from "styled-components"
+import Contact from "./components/Contact"
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
+
+const Container = styled.div`
+height:100vh;
+scroll-snap-type: y mandatory;
+scroll-behavior:smooth;
+overflow-y:auto;
+scrollbar-width:none;
+color:white;
+background:url("./img/ ");
+&::-webkit-scrollbar{
+  display:none;
+}
+
+`;
+
+function App(){
+
+  return (
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/Works.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+height:100vh;
+background-color:#85c994;
+scroll-snap-align:center;
+`
+
+const Works = () => {
+  return (
+    <Section>
+      Works
+    </Section>
+  )
+}
+
+export default Works
